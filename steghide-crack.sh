2d2d99e1d3972d1fe3f08b2177d0a43c6c40ae6f
Felipe <felipe.citnnet@gmail.com>
2017-11-18 16:51:33 -0200

    Add files via upload

---
+++ steghide-crack.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#EL1S1uM Was Here
+
+if [ $# != 2 ]; then
+	echo "USAGE: $0 imagem.jpg wordlist.txt"
+else
+
+for senha in $(cat $2); do
+	steghide extract -sf $1 -p $senha -xf output.txt &> /dev/null
+	if [ $? == 0 ]; then
+		echo
+		echo "SUCESS - $senha"
+		echo "FILE: output.txt"
+		break
+	else
+		echo "FAILED - $senha"
+	fi
+done
+fi
